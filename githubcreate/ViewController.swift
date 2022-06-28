2d901a4af0c9218e040577fb2cf508dd08b37adf
garance <garance.baron1@gmail.com>
2022-06-28 23:12:26 +0700

    Initial Commit

---
+++ githubcreate/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  githubcreate
+//
+//  Created by 25GaranceB on 28/6/22.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
